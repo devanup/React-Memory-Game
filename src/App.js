ca918a8241e114efea5b1c843c3e5f0ac2dfdeb4
Dev Anup <anupthedev@gmail.com>
2023-03-24 16:25:13 -0400

    Added sound, improved code, got rid of bugs, fixed counter, fixed new game button, styled theme toggler

--- src/App.js
+++ src/App.js
@@ -6,8 +6,13 @@ import {
 	faMoon,
 	faVolumeHigh,
 	faVolumeMute,
+	faVolumeUp,
 } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
+import clickSound from './assets/card-flip.mp3';
+import matchSound from './assets/card-match.mp3';
+import winSound from './assets/win-sound.mp3';
+import './util.js';
 
 const shuffle = (array) => {
 	// Loop through the array from the end to the beginning
@@ -30,40 +35,37 @@ const NewGameButton = ({ onClick }) => {
 	);
 };
 
-const Grid = ({ refProp }) => {
-	// Set up original cards and three state variables
+const Grid = ({ refProp, setMoves, setAllMatched }) => {
 	const originalCards = ['🐶', '🐱', '🐻', '🐨', '🦁', '🐯'];
-
 	const [cards, setCards] = useState([]);
 	const [flippedCards, setFlippedCards] = useState([]);
 	const [matchedCards, setMatchedCards] = useState([]);
 
-	// Set up effect to shuffle the cards on mount
-	useEffect(() => {
-		// Create pairs of each emoji
-		const emojiPairs = originalCards.flatMap((emoji) => [emoji, emoji]);
+	const clickAudio = new Audio(clickSound);
+	const matchAudio = new Audio(matchSound);
+	const winAudio = new Audio(winSound);
 
-		// Randomly shuffle the emoji pairs
+	useEffect(() => {
+		const emojiPairs = originalCards.flatMap((emoji) => [emoji, emoji]);
 		const shuffledPairs = shuffle(emojiPairs);
 		setCards(shuffledPairs);
 	}, []);
 
 	const startNewGame = () => {
-		// Reset flipped and matched cards after 100ms
 		setTimeout(() => {
 			setFlippedCards([]);
 			setMatchedCards([]);
 		}, 100);
 
-		// Shuffle cards again after 700ms
 		setTimeout(() => {
 			const emojiPairs = originalCards.flatMap((emoji) => [emoji, emoji]);
 			const shuffledPairs = shuffle(emojiPairs);
 			setCards(shuffledPairs);
 		}, 700);
+		setMoves(0); // reset move count
+		setAllMatched(false);
 	};
 
-	// Check if two flipped cards match
 	useEffect(() => {
 		if (flippedCards.length === 2) {
 			const [firstCard, secondCard] = flippedCards;
@@ -74,13 +76,26 @@ const Grid = ({ refProp }) => {
 					firstCard,
 					secondCard,
 				]);
+				setTimeout(() => {
+					matchAudio.play();
+				}, 750);
 			}
-			// Reset flipped cards after 700ms
+
 			setTimeout(() => {
 				setFlippedCards([]);
+				if (cards[firstCard] === cards[secondCard]) {
+					setMoves((prevMoves) => prevMoves + 1);
+				}
 			}, 700);
 		}
-	}, [flippedCards]);
+
+		if (matchedCards.length === cards.length && matchedCards.length !== 0) {
+			setTimeout(() => {
+				setAllMatched(true);
+				winAudio.play();
+			}, 800);
+		}
+	}, [flippedCards, cards, setMoves]);
 
 	// Handle card click
 	const handleClick = (index) => {
@@ -91,6 +106,12 @@ const Grid = ({ refProp }) => {
 			return;
 		}
 		setFlippedCards((prevFlippedCards) => [...prevFlippedCards, index]);
+
+		clickAudio.play(); // play audio
+
+		if (flippedCards.length === 1 && cards[flippedCards[0]] !== cards[index]) {
+			setMoves((prevMoves) => prevMoves + 1);
+		}
 	};
 
 	// Set up ref to pass startNewGame function to header component
@@ -129,7 +150,7 @@ const Grid = ({ refProp }) => {
 	);
 };
 
-const Header = ({ gridRef }) => {
+const Header = ({ gridRef, moves }) => {
 	// Function to handle click on the New Game button
 	const handleClick = () => {
 		gridRef.current.startNewGame(); // Calls the startNewGame method of the Grid component
@@ -141,19 +162,36 @@ const Header = ({ gridRef }) => {
 				Match 'Em
 			</a>
 			<table>
-				<tr>
-					<td>
-						<NewGameButton onClick={handleClick} />
-					</td>
-					<td rowSpan='2'>
-						<div className='moves'>0 moves</div>
-						<div className='timer' id='timer'>
-							00:00:00
-						</div>
-					</td>
-				</tr>
+				<tbody>
+					<tr>
+						<td>
+							<div className='moves'>
+								{moves} {moves > 1 ? 'moves' : 'move'}
+							</div>
+						</td>
+						<td>
+							<NewGameButton onClick={handleClick} />
+						</td>
+					</tr>
+				</tbody>
 			</table>
 		</header>
+	);
+};
+
+const Modal = ({ moves, allMatched }) => {
+	// console.log(`${moves} moves`);
+	// console.log(`All matched?: ${allMatched}`);
+	return (
+		<div className={`modal ${allMatched === true ? 'showModal' : ''}`}>
+			<span role='img' aria-label='celerbate'>
+				🥳
+			</span>
+			<div className='modal-wrap'>
+				<h1>You've matched 'em all!</h1>
+				<p>{`${moves} ${moves > 1 ? 'moves' : 'move'}`}</p>
+			</div>
+		</div>
 	);
 };
 
@@ -171,54 +209,78 @@ const Credit = () => (
 );
 
 const Settings = () => (
-	<div className='settings menuOff' id='settings'>
-		{/* <i className='fa fa-cog' id='settings-icon'></i> */}
-		<FontAwesomeIcon icon={faCog} className='fa' id='settings-icon' />
-
-		<div className='theme-toggle-wrap' id='theme-toggle-wrap'>
-			<div className='sound-toggle' id='sound-toggle'>
-				<div className='sound-icon'>
-					{/* <i className='fa-solid fa-volume-high soundOn' id='sound-on-icon'></i>
-					<i className='fa-solid fa-volume-xmark' id='sound-off-icon'></i> */}
-					<FontAwesomeIcon
-						icon={faVolumeHigh}
-						className='fa-solid fa-volume-high soundOn'
-						id='sound-on-icon'
-					/>
-					<FontAwesomeIcon
-						icon={faVolumeMute}
-						className='fa-solid fa-volume-xmark'
-						id='sound-off-icon'
-					/>
-				</div>
-			</div>
-			<div className='theme-toggle' id='theme-toggle'>
-				<div className='active-bg' id='active-bg'></div>
-				<div className='light-icon active' id='light-icon'>
-					{/* <i className='fas fa-sun'></i> */}
-					<FontAwesomeIcon icon={faSun} className='fas fa-sun' />
-				</div>
-				<div className='dark-icon' id='dark-icon'>
-					{/* <i className='fas fa-moon'></i> */}
-					<FontAwesomeIcon icon={faMoon} className='fas fa-moon' />
-				</div>
-			</div>
+	<div className='theme-toggle' id='theme-toggle'>
+		<div className='active-bg' id='active-bg'></div>
+		<div className='light-icon active' id='light-icon'>
+			{/* <i className='fas fa-sun'></i> */}
+			<FontAwesomeIcon icon={faSun} className='fas fa-sun' />
+		</div>
+		<div className='dark-icon' id='dark-icon'>
+			{/* <i className='fas fa-moon'></i> */}
+			<FontAwesomeIcon icon={faMoon} className='fas fa-moon' />
 		</div>
 	</div>
 );
 
+const SoundButton = () => {
+	const [soundEnabled, setSoundEnabled] = useState(true);
+
+	const handleClick = () => {
+		setSoundEnabled(!soundEnabled);
+		console.log('Sound: ', soundEnabled);
+	};
+
+	return (
+		<button
+			className='sound-button'
+			onClick={handleClick}
+			style={{
+				position: 'fixed',
+				bottom: '50px',
+				right: '50px',
+				backgroundColor: '#333',
+				borderRadius: '50%',
+				width: '60px',
+				height: '60px',
+				outline: 'none',
+				border: 'none',
+				cursor: 'pointer',
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+			}}
+		>
+			<FontAwesomeIcon
+				icon={soundEnabled ? faVolumeUp : faVolumeMute}
+				style={{ color: '#fff', fontSize: '24px' }}
+			/>
+		</button>
+	);
+};
+
 // Main component that renders the game.
 function App() {
-	// create a reference to the grid component
 	const gridRef = useRef(null);
-
+	const [moves, setMoves] = useState(0);
+	const [allMatched, setAllMatched] = useState(false);
+	// const [soundEnabled, setSoundEnabled] = useState(true);
+	// console.log(moves);
 	return (
 		<div className='App'>
 			<div className='game-container'>
-				<Header gridRef={gridRef} />
-				<Grid refProp={gridRef} />
+				<Header gridRef={gridRef} moves={moves} />
+				<Grid
+					refProp={gridRef}
+					setMoves={setMoves}
+					setAllMatched={setAllMatched}
+				/>
 				<Credit />
 				<Settings />
+				<Modal moves={moves} allMatched={allMatched} />
+				{/* <SoundButton
+					soundEnabled={soundEnabled}
+					setSoundEnabled={setSoundEnabled}
+				/> */}
 			</div>
 		</div>
 	);
